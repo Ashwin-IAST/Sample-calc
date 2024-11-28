ee7fab17e6e98ebdc8986e2807c973acf41311e1
Ashwin-IAST <ashwin.krishna@iast-software.com>
2024-11-28 10:19:03 +0530

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,12 @@
+// calculator.h
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+// Function prototypes for calculator operations
+void add(float num1, float num2);
+void subtract(float num1, float num2);
+void multiply(float num1, float num2);
+void divide(float num1, float num2);
+
+#endif // CALCULATOR_H
