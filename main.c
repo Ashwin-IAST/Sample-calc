4d7b9c1c4fa247c07b577d731269298bacab2544
Ashwin-IAST <ashwin.krishna@iast-software.com>
2024-11-28 10:47:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -26,4 +26,27 @@ int main() {
         // Take input for numbers
         printf("Enter first number: ");
         scanf("%f", &num1);
-        printf("Enter seco
+        printf("Enter second number: ");
+        scanf("%f", &num2);
+
+        // Perform operation based on user choice
+        switch (choice) {
+            case 1:
+                add(num1, num2);
+                break;
+            case 2:
+                subtract(num1, num2);
+                break;
+            case 3:
+                multiply(num1, num2);
+                break;
+            case 4:
+                divide(num1, num2);
+                break;
+            default:
+                printf("Invalid choice! Please select a valid operation.\n");
+        }
+    }
+
+    return 0;
+}
